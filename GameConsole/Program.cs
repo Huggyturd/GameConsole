2c3345aff88bad1579ccea3814df902ee389a35b
CLEITON MOURA LOURA <cleitonmloura@gmail.com>
2018-11-17 13:27:12 -0200

    Accessing and Checking for Null Values : Using the Null-conditional Operator with Null Array Values

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -10,15 +10,18 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            PlayerCharacter player = new PlayerCharacter();
-            player.DaysSinceLastLogin = 42;
+            //PlayerCharacter[] players = new PlayerCharacter[3]
+            //{
+            //    new PlayerCharacter {Name = "Sarah"},
+            //    new PlayerCharacter(),
+            //    null
+            //};
 
-            int days = player?.DaysSinceLastLogin ?? -1;
-            
+            PlayerCharacter[] players = null;
 
-            //int days = player.DaysSinceLastLogin.Value;
-
-            Console.Write(days);
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
 
             Console.ReadLine();
         }
