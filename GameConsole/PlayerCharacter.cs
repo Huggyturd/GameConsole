43854070c43746425c9abff7ccd18f80c614da5d
CLEITON MOURA LOURA <cleitonmloura@gmail.com>
2018-11-19 10:10:16 -0200

    Eliminating Null Reference Exceptions : Implementing the Null Object Pattern Using Interfaces

--- GameConsole/PlayerCharacter.cs
+++ GameConsole/PlayerCharacter.cs
@@ -8,10 +8,33 @@ namespace GameConsole
 {
     class PlayerCharacter
     {
-        public string Name { get; set; }
-        public int? DaysSinceLastLogin { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public bool? IsNoob { get; set; }
+        private readonly ISpecialDefence _specialDefence;
 
+        public PlayerCharacter(ISpecialDefence specialDefence)
+        {
+            _specialDefence = specialDefence;
+        }
+
+        public string Name { get; set; }
+
+        public int Health { get; set; } = 100;
+
+        public void Hit(int damage)
+        {
+            //int damageReduction = 0;
+
+            //if (_specialDefence != null)
+            //{
+            //    damageReduction = _specialDefence.CalculateDamageReduction(damage);
+            //}
+
+            //int totalDamageTaken = damage - damageReduction;
+
+            int totalDamageTaken = damage - _specialDefence.CalculateDamageReduction(damage);
+
+            Health -= totalDamageTaken;
+
+            Console.WriteLine($"{Name}'s helath has been reduced by {totalDamageTaken} to {Health}");
+        }
     }
 }
--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -10,18 +10,24 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            //PlayerCharacter[] players = new PlayerCharacter[3]
-            //{
-            //    new PlayerCharacter {Name = "Sarah"},
-            //    new PlayerCharacter(),
-            //    null
-            //};
+            PlayerCharacter sarah = new PlayerCharacter(new DiamondSkinDefence())
+            {
+                Name = "Sarah"
+            };
 
-            PlayerCharacter[] players = null;
+            PlayerCharacter amrit = new PlayerCharacter(new IronBonesDefence())
+            {
+                Name = "Amrit"
+            };
 
-            string p1 = players?[0]?.Name;
-            string p2 = players?[1]?.Name;
-            string p3 = players?[2]?.Name;
+            PlayerCharacter gentry = new PlayerCharacter(new NullDefence())
+            {
+                Name = "Gentry"
+            };
+
+            sarah.Hit(10);
+            amrit.Hit(10);
+            gentry.Hit(10);
 
             Console.ReadLine();
         }
